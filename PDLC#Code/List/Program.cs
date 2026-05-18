8bb93cc327aa9684924045d1b79a67f4cd8e0f22
Shifat <shifatnahid91@gmail.com>
2026-05-18 11:50:39 +0600

    Remove korar code

--- PDLC#Code/List/Program.cs
+++ PDLC#Code/List/Program.cs
@@ -1,8 +1,22 @@
-﻿List<int> number = new List<int> { 10, 20,  30 };
+﻿List<int> number = new List<int> { 10, 20,  30};
+
+//Add number
 number.Add(40);
 number.Add(50);
 
-foreach(var item  in number)
+foreach (var item in number)
+{
+    Console.Write(" " + item);
+}
+
+Console.WriteLine();
+
+//Remove number
+number.Remove(30);
+number.Remove(50);
+
+foreach(var item in number)
 {
     Console.Write(" "+item);
 }
+
