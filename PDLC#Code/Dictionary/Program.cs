d772583c1bff37abe05bc77122d04ec5cbfe205b
Shifat <shifatnahid91@gmail.com>
2026-05-18 12:58:18 +0600

    Dictionary all code

---
+++ PDLC#Code/Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿//Dictionary<keyType, ValueType>
+
+Dictionary<string, int> marks = new Dictionary<string, int>();
+
+//Adding
+marks.Add("Shifat", 90);
+marks.Add("Nahid", 87);
+marks.Add("Jamil", 92);
+
+//Value Read 
+Console.WriteLine(marks["Shifat"]);
+
+//Update
+marks["Jamil"] = 94;
+
+//Remove
+marks.Remove("Nahid");
+
+//All data show
+foreach (var mark in marks)
+{
+    Console.WriteLine(mark.Key + "-->"+ mark.Value);
+}
+
+//Key searching
+if (marks.ContainsKey("Shifat"))
+{
+    Console.WriteLine("Key ta holo " + marks["Shifat"]);
+}
+else
+{
+    Console.WriteLine("Key nei");
+}
