6757f43f1f55ef5016d38b1b5284281298d4ed3e
Shifat <shifatnahid91@gmail.com>
2026-05-18 11:13:35 +0600

    Boxing and Unboxing with ArrayList code

--- PDLC#Code/Boxing and Unboxing with ArrayList/Program.cs
+++ PDLC#Code/Boxing and Unboxing with ArrayList/Program.cs
@@ -1,5 +1,7 @@
 ﻿//Boxing 
 //Primitive type (int, bool, double) → object এ Convert
+using System.Collections;
+
 int num = 12; //int(stack e thake)
 object obj = num;//Boxing --> int ke object e vorlam(Heap e gelo)
 Console.WriteLine("Boxing: "+obj);
@@ -9,3 +11,21 @@ Console.WriteLine("Boxing: "+obj);
 object obj1 = 45;
 int num1 = (int)obj1;
 Console.WriteLine("Unboxing: "+num1);
+
+//Boxing and Unboxing with ArrayList
+ArrayList arrayList = new ArrayList();
+
+//Boxing(int --> Obj)
+arrayList.Add(10);
+arrayList.Add(20.23);
+arrayList.Add(true);
+
+//Unboxing(obj --> int)
+
+int x = (int)arrayList[0];
+double d = (double)arrayList[1];
+bool b = (bool)arrayList[2];
+
+Console.WriteLine("1st Element: " + x);
+Console.WriteLine("2nd Element: "+ d);
+Console.WriteLine("3rd Element: " + b);
